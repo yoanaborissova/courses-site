991ca73760397183e6f2a614b0e8a76de3451b6f
yoanaborissova <yoana.borissova@gmail.com>
2019-08-10 18:41:38 +0300

    Addded educations functionality

--- scripts/EducationsComponent.js
+++ scripts/EducationsComponent.js
@@ -6,7 +6,34 @@ class EducationsComponent extends Component {
 
     attachEvents() {
         document.querySelectorAll('.expand').forEach(a => {
-            a.onclick = () => console.log('clicked expand!');
+            a.onclick = (e) => {
+
+                let id = e.path[2].id;
+
+                let startDelay = 0;
+
+                for (let li of document.getElementById(id + '-list').children) {
+                    startDelay += 0.2;
+
+                    li.setAttribute('style', 'animation-delay: ' + startDelay + 's');
+                }
+
+                if (document.getElementById(id + '-list')
+                    .getAttribute('style') === 'display: none') {
+                    document.getElementById(id + '-list')
+                        .setAttribute('style', 'display: block');
+                    document.getElementById(id)
+                        .getElementsByTagName('img')[0]
+                        .setAttribute('style', 'transform: rotate(180deg)');
+                } else {
+                    document.getElementById(id + '-list')
+                        .setAttribute('style', 'display: none');
+                    document.getElementById(id)
+                        .getElementsByTagName('img')[0]
+                        .setAttribute('style', 'transform: rotate(0deg)');
+                }
+
+            };
         })
     }
 }
