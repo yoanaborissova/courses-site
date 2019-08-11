963c21b0923a8259ada2d0479fe82e534f60976c
yoanaborissova <yoana.borissova@gmail.com>
2019-08-11 13:34:18 +0300

    code standards fixes

--- app/scripts/EducationsComponent.js
+++ app/scripts/EducationsComponent.js
@@ -6,39 +6,23 @@ class EducationsComponent extends Component {
 
     attachEvents() {
         document.querySelectorAll('.expand').forEach(a => {
-            a.onclick = (e) => {
-                let id;
-
-                if (e.path){
-                    id = e.path[2].id;
-                } else if (e.originalTarget) {
-                    id = e.originalTarget.parentElement.parentElement.attributes['id'].value;
-                } else {
-                    id = e.target.parentElement.attributes[0].ownerElement.parentElement.id;
-                }
+            a.onclick = e => {
+                let id = e.target.dataset.id;
 
                 let startDelay = 0;
 
-                for (let li of document.getElementById(id + '-list').children) {
+                for (let li of document.getElementById(id).children) {
                     startDelay += 0.2;
 
                     li.setAttribute('style', 'animation-delay: ' + startDelay + 's');
                 }
 
-                if (document.getElementById(id + '-list')
-                    .getAttribute('style') !== 'display: block')
-                {
-                    document.getElementById(id + '-list')
-                        .setAttribute('style', 'display: block');
-                    document.getElementById(id)
-                        .getElementsByTagName('img')[0]
-                        .setAttribute('style', 'transform: rotate(180deg)');
+                if (document.getElementById(id).getAttribute('style') !== 'display: block') {
+                    document.getElementById(id).setAttribute('style', 'display: block');
+                    e.target.setAttribute('style', 'transform: rotate(180deg)');
                 } else {
-                    document.getElementById(id + '-list')
-                        .setAttribute('style', 'display: none');
-                    document.getElementById(id)
-                        .getElementsByTagName('img')[0]
-                        .setAttribute('style', 'transform: rotate(0deg)');
+                    document.getElementById(id).setAttribute('style', 'display: none');
+                    e.target.setAttribute('style', 'transform: rotate(0deg)');
                 }
 
             };
